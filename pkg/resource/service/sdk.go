ce2d827ca664f4271c1212d82cb9a572613c5812
Trevor Knottnerus <tknott@amazon.com>
2026-06-11 00:22:09 -0700

    Fix CapacityProviderStrategy update path
    - Add hook to set ForceNewDeployment when updating CapacityProviderStrategy

--- pkg/resource/service/sdk.go
+++ pkg/resource/service/sdk.go
@@ -2023,6 +2023,12 @@ func (rm *resourceManager) sdkUpdate(
 	if desired.ko.Spec.Name != nil {
 		input.Service = desired.ko.Spec.Name
 	}
+	// ECS requires ForceNewDeployment when modifying the capacity provider
+	// strategy on an existing service, otherwise the API returns
+	// InvalidParameterException.
+	if delta.DifferentAt("Spec.CapacityProviderStrategy") {
+		input.ForceNewDeployment = true
+	}
 
 	var resp *svcsdk.UpdateServiceOutput
 	_ = resp
